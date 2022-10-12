fe48fa3576fd6ecd4b13a82cbb5e8f5d3af3eca9
uTakCouDeT <b20030021@gmail.com>
2022-10-12 23:29:02 +0300

    Cache for Windows

---
+++ MO-3/cmake-build-debug/CMakeFiles/MO_3.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/MO_3.dir/main.cpp.obj"
+  "CMakeFiles/MO_3.dir/main.cpp.obj.d"
+  "MO_3.exe"
+  "MO_3.exe.manifest"
+  "MO_3.pdb"
+  "libMO_3.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/MO_3.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
