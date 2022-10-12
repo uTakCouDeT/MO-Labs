1416ab232891cb092ef3dca8097486b0e6804a36
uTakCouDeT <b20030021@gmail.com>
2022-10-12 10:46:30 +0300

    aboba

---
+++ MO-1/cmake-build-debug/CMakeFiles/MO_1.dir/compiler_depend.make
@@ -0,0 +1,2 @@
+# Empty compiler generated dependencies file for MO_1.
+# This may be replaced when dependencies are built.
---
+++ MO-2/cmake-build-debug/CMakeFiles/MO_2.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/MO_2.dir/main.cpp.o"
+  "CMakeFiles/MO_2.dir/main.cpp.o.d"
+  "MO_2"
+  "MO_2.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/MO_2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
